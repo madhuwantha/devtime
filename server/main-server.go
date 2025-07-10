c835abd9dfa925bb34b384c83135f6568221571a
madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2025-07-10 16:54:59 +0530

    add API routing

--- server/main-server.go
+++ server/main-server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/madhuwantha/devtime/mongostorage"
 )
@@ -12,5 +13,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+	r := gin.Default()
 	mongostorage.Connect()
+
+	devtime := r.Group("/api")
+	{
+		devtime.GET("/logs", func(c *gin.Context) {})
+		devtime.POST("/logs", func(c *gin.Context) {})
+
+	}
+
+	r.Run()
+
 }
