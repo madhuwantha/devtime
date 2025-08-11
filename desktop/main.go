566e35652f9d1803b0dcdcafcb0a3c46243f1987
Madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2025-08-11 22:01:45 +0530

    title bar

--- desktop/main.go
+++ desktop/main.go
@@ -2,43 +2,47 @@ package main
 
 import (
 	"embed"
+	"log"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
+//go:embed build/appicon.png
+var icon []byte
+
 func main() {
 
-	// Detect if we're running a CLI command
-	// if len(os.Args) > 1 {
-	// 	// run cobra CLI directly
-	// 	cmd.Execute()
-	// 	return
-	// }
-
-	// Create an instance of the app structure
 	app := NewApp()
 
-	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "desktop",
-		Width:  568,
-		Height: 768,
+		Title:             "Devtime",
+		Width:             568,
+		Height:            768,
+		BackgroundColour:  &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		StartHidden:       false, // Set to true if you want to start minimized
+		HideWindowOnClose: true,  // Hide instead of quit when window is closed
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
+		OnStartup: app.startup,
+		Bind:      []interface{}{app},
+
+		Mac: &mac.Options{
+			About: &mac.AboutInfo{
+				Title:   "My Application",
+				Message: "© 2021 Me",
+				Icon:    icon,
+			},
 		},
 	})
 
 	if err != nil {
-		println("Error:", err.Error())
+		log.Fatal("Error:", err)
 	}
 }
